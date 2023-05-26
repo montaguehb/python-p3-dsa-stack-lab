76bb8d4a861ddaba04c50195fe42f0ead57a536d
Historia Montague <historia.montague@outlook.com>
2023-05-26 11:51:18 -0600

    Finished Lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,35 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items = None, limit = 100):
+        self.limit = limit
+        self.items = []
+        if items:
+            for item in items:
+                self.push(item)
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if(self.isEmpty()):
+            return
+        return self.items.pop()
 
     def peek(self):
-        pass
+        return self.items[-1]
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        try:
+            return len(self.items) - self.items.index(target) - 1
+        except ValueError:
+            return -1 
